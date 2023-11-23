e68c868576a3632e34c159b08402dedc3f2a9424
AJMCoder <alexmullins@hotmail.co.uk>
2023-11-23 11:31:51 +0000

    Add more spaces to the board to make more clear. Change code to allow different symbols if required.

--- run.py
+++ run.py
@@ -13,9 +13,7 @@ print("If you sink a ship, you will be told that you have sunk a ship.")
 print("If the computer sinks one of your ships, you will be told that the computer has sunk one of your ships.")
 print("The game will end when either you or the computer has sunk all of the other's ships.")
 print("Good luck!")
-print("  0 1 2 3 4 5 6 7 8 9")
-for x in range(10):
-    print(x, "_ "*10)
+
 print("You will now be asked to place your ships.")
 print("You will be asked to enter the coordinates of where you want to place your ships.")
 print("You will be asked to enter the coordinates of the front of the ship.")
@@ -25,3 +23,13 @@ print("The computer will randomly generate the coordinates of the front of the s
 print("The computer will then randomly generate the coordinates of the back of the ship.")
 print("The computer will then check if the ship is in a valid position.")
 print("If the ship is in a valid position, it will be placed.")
+print("     0  1  2  3  4  5  6  7  8  9")
+
+#outside for loop prints each row, number of row and the string of dashes.
+for x in range(10):
+    row = ""
+#inside for loop creates the row of dashes.
+    for y in range(10):
+        chr = " _ "
+        row = row + chr
+    print(x, " ",row)
